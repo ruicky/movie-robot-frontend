40e96c782fdad90d757e72871fe98aef21e9dcb5
yipengfei <yipengfei329@gmail.com>
2022-03-20 15:39:49 +0800

    上传和下载的文件尺寸忽略负值

--- src/utils/PtUtils.js
+++ src/utils/PtUtils.js
@@ -1,4 +1,7 @@
 export const coverSize = (size_mb) => {
+    if (size_mb < 0) {
+        return "0"
+    }
     if (size_mb <= 1024) {
         return size_mb + "MB";
     } else if (size_mb <= 1024 * 1024) {
