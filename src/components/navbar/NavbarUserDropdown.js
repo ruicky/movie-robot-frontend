ed525c0f12026ae695372e53aca7880de2480478
yipengfei <yipengfei329@gmail.com>
2022-04-07 16:39:20 +0800

    去掉执行豆瓣任务操作

--- src/components/navbar/NavbarUserDropdown.js
+++ src/components/navbar/NavbarUserDropdown.js
@@ -1,13 +1,8 @@
 import * as React from "react";
 import styled from "styled-components/macro";
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
-import {
-  Tooltip,
-  Menu,
-  MenuItem,
-  IconButton as MuiIconButton,
-} from "@mui/material";
+import {IconButton as MuiIconButton, Menu, MenuItem, Tooltip,} from "@mui/material";
 
 
 import useAuth from "@/hooks/useAuth";
@@ -25,62 +20,65 @@ const IconButton = styled(MuiIconButton)`
 `;
 
 function NavbarUserDropdown() {
-  const [anchorMenu, setAnchorMenu] = React.useState(null);
-  const navigate = useNavigate();
-  const { signOut } = useAuth();
+    const [anchorMenu, setAnchorMenu] = React.useState(null);
+    const navigate = useNavigate();
+    const {signOut} = useAuth();
 
-  const theme = useStore(state=>state.theme)
+    const theme = useStore(state => state.theme)
 
-  const toggleMenu = (event) => {
-    setAnchorMenu(event.currentTarget);
-  };
+    const toggleMenu = (event) => {
+        setAnchorMenu(event.currentTarget);
+    };
 
-  const closeMenu = () => {
-    setAnchorMenu(null);
-  };
+    const closeMenu = () => {
+        setAnchorMenu(null);
+    };
 
-  const handleSignOut = async () => {
-    await signOut();
-    navigate("/auth/sign-in");
-  };
-  const runTaskNow = ()=>{
-    axios
-      .get("/api/task/run", {
-        params: {
-          name: "sync_movies",
-        },
-      })
-      .then(({ code, message }) => {
-        msg.success(message)
-        closeMenu()
-      });
-  }
+    const handleSignOut = async () => {
+        await signOut();
+        navigate("/auth/sign-in");
+    };
+    const runTaskNow = (name) => {
+        axios
+            .get("/api/task/run", {
+                params: {
+                    name: name,
+                },
+            })
+            .then(({code, message}) => {
+                msg.success(message)
+                closeMenu()
+            });
+    }
 
-  return (
-    <React.Fragment>
-      <Tooltip title="系统">
-        <IconButton
-          aria-owns={Boolean(anchorMenu) ? "menu-appbar" : undefined}
-          aria-haspopup="true"
-          onClick={toggleMenu}
-          color="inherit"
-          size="large"
-        >
-          <MoreVert />
-        </IconButton>
-      </Tooltip>
-      <Menu
-        id="menu-appbar"
-        anchorEl={anchorMenu}
-        open={Boolean(anchorMenu)}
-        onClose={closeMenu}
-      >
-        <MenuItem onClick={()=>{theme.toggle(true);closeMenu()}}>更换主题</MenuItem>
-        <MenuItem onClick={runTaskNow}>执行豆瓣任务</MenuItem>
-        <MenuItem onClick={handleSignOut}>退出</MenuItem>
-      </Menu>
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <Tooltip title="系统">
+                <IconButton
+                    aria-owns={Boolean(anchorMenu) ? "menu-appbar" : undefined}
+                    aria-haspopup="true"
+                    onClick={toggleMenu}
+                    color="inherit"
+                    size="large"
+                >
+                    <MoreVert/>
+                </IconButton>
+            </Tooltip>
+            <Menu
+                id="menu-appbar"
+                anchorEl={anchorMenu}
+                open={Boolean(anchorMenu)}
+                onClose={closeMenu}
+            >
+                <MenuItem onClick={() => {
+                    theme.toggle(true);
+                    closeMenu()
+                }}>更换主题</MenuItem>
+              <MenuItem onClick={runTaskNow('smart_download_task')}>执行智能下载任务</MenuItem>
+                <MenuItem onClick={handleSignOut}>退出</MenuItem>
+            </Menu>
+        </React.Fragment>
+    );
 }
 
 export default NavbarUserDropdown;
