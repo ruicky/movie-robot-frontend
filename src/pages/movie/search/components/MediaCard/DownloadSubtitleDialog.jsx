5c187c16f96e2fabffb8ff7ac50608ebef5d8027
yipengfei <yipengfei329@gmail.com>
2022-05-30 12:18:38 +0800

    增加字幕下载按钮

---
+++ src/pages/movie/search/components/MediaCard/DownloadSubtitleDialog.jsx
@@ -0,0 +1,59 @@
+import ClosedCaptionIcon from "@mui/icons-material/ClosedCaption";
+import {Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle} from "@mui/material";
+import React, {useEffect, useState} from "react";
+import {useSubmitDownloadSubtitle} from "@/api/TaskApi";
+import message from "@/utils/message";
+
+function DownloadSubtitleDialog({tmdb_id, media_type, season_index = null, episode_index = null}) {
+    const [open, setOpen] = useState(false);
+    const {mutate: submitTask, isLoading} = useSubmitDownloadSubtitle();
+    const handleSubmit = () => {
+        submitTask({
+            tmdb_id,
+            media_type,
+            season_index,
+            episode_index: episode_index ? episode_index.join(',') : null
+        }, {
+            onSuccess: resData => {
+                const {code, message: msg} = resData;
+                if (code === 0) {
+                    message.success(msg);
+                } else {
+                    message.error(msg);
+                }
+                setOpen(false)
+            },
+            onError: error => message.error(error)
+        })
+    }
+    return (
+        <>
+            <Button variant="outlined" startIcon={<ClosedCaptionIcon/>} onClick={() => setOpen(true)}>
+                下载字幕
+            </Button>
+            <Dialog
+                open={open}
+                onClose={() => setOpen(false)}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {'操作提示'}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        确定要立即启动一个新的字幕下载任务么？
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setOpen(false)}>取消</Button>
+                    <Button onClick={handleSubmit} autoFocus disabled={isLoading}>
+                        确定
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </>
+    )
+}
+
+export default DownloadSubtitleDialog;
