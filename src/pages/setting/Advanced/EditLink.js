0b47119a32fb6d455489badcf6c1339786b0ea3f
yipengfei <yipengfei329@gmail.com>
2022-06-15 15:50:05 +0800

    增加全局文件处理模式设置

--- src/pages/setting/Advanced/EditLink.js
+++ src/pages/setting/Advanced/EditLink.js
@@ -7,10 +7,14 @@ import {
     Button,
     Checkbox,
     Divider as MuiDivider,
+    FormControl,
     FormControlLabel,
     FormGroup,
+    FormHelperText,
     FormLabel,
     Link,
+    MenuItem,
+    Select,
     TextField as MuiTextField,
     Typography
 } from "@mui/material";
@@ -18,7 +22,7 @@ import styled from "styled-components/macro";
 import {spacing} from "@mui/system";
 import {useFormik} from "formik";
 import * as Yup from "yup";
-import {useGetLinkSetting, useGetServerSetting, useSaveLinkSetting, useSaveServerSetting} from "@/api/SettingApi";
+import {useGetLinkSetting, useSaveLinkSetting} from "@/api/SettingApi";
 import message from "@/utils/message";
 
 const Divider = styled(MuiDivider)(spacing);
@@ -42,7 +46,8 @@ function EditForm({}) {
             tv_folder_format: "",
             recognize: true,
             exact_year: true,
-            use_unknown_dir: true
+            use_unknown_dir: true,
+            file_process_mode: "link"
         }, validationSchema: Yup.object().shape({
             movie_filename_format: Yup.string().required("电影名称模版不能为空"),
             movie_folder_format: Yup.string().required("电影文件夹模版不能为空"),
@@ -80,12 +85,24 @@ function EditForm({}) {
             formik.setFieldValue("recognize", setting.data?.recognize !== undefined && setting.data?.recognize !== null ? setting.data?.recognize : true);
             formik.setFieldValue("exact_year", setting.data?.exact_year !== undefined && setting.data?.exact_year !== null ? setting.data?.exact_year : true);
             formik.setFieldValue("use_unknown_dir", setting.data?.use_unknown_dir !== undefined && setting.data?.use_unknown_dir !== null ? setting.data?.use_unknown_dir : true);
+            formik.setFieldValue("file_process_mode", setting.data?.file_process_mode ? setting.data?.file_process_mode : 'link');
         }
     }, [setting]);
     return (<form noValidate onSubmit={formik.handleSubmit}>
         {formik.errors.submit && (<Alert mt={2} mb={1} severity="warning">
             {formik.errors.submit}
         </Alert>)}
+        <FormControl fullWidth>
+            <Select
+                name="file_process_mode"
+                value={formik.values.file_process_mode}
+                onChange={formik.handleChange}
+            >
+                <MenuItem value="link">硬链接到目标路径</MenuItem>
+                <MenuItem value="copy">复制到目标路径</MenuItem>
+            </Select>
+            <FormHelperText>默认全局文件处理模式，细分目录还是可以在媒体文件夹设置</FormHelperText>
+        </FormControl>
         <FormLabel component="legend">电影整理文件格式</FormLabel>
         <FormGroup>
             <TextField
@@ -95,11 +112,19 @@ function EditForm({}) {
                 value={formik.values.movie_filename_format}
                 error={Boolean(formik.touched.movie_filename_format && formik.errors.movie_filename_format)}
                 fullWidth
-                helperText="最终整理后文件名格式"
+                helperText={(
+                    <span>
+                        最终整理后文件名格式，采用Jinja2模版
+                        <Link target="_blank"
+                              href="https://yee329.notion.site/ed17e503e361449e8f364104d64ab150">
+                            去学习如何使用
+                        </Link>
+                    </span>
+                )}
                 onBlur={formik.handleBlur}
                 onChange={formik.handleChange}
                 my={3}
-            />
+                />
             <TextField
                 type="text"
                 name="movie_folder_format"
