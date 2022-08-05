45c0e9e4201318832edb7b8c883cf3d74beb3aef
wangruirui@ruijie.com.cn <wangruirui>
2022-08-05 16:29:15 +0800

    fix: 菜单滚动条显示隐藏问题

--- src/components/SearchDialog/SearchTemplate.jsx
+++ src/components/SearchDialog/SearchTemplate.jsx
@@ -5,12 +5,13 @@ import styled from "styled-components/macro";
 
 const SearchTemplate = ({templates, templateType, setTemplateType}) => {
     return (
-        <Stack spacing={2} direction="row" justifyContent="left" sx={{mb: 2}}>
+        <Stack spacing={2} direction="row"  sx={{mb: 2}}>
             {
                 templates && templates.map(
                     item =>
                         <Button
                             key={item.type}
+                            sx={{whiteSpace: 'nowrap', flexShrink: 0}}
                             variant={templateType === item.type ? 'contained' : 'text'}
                             onClick={() => setTemplateType(item.type)}>
                             {item.name}
@@ -26,13 +27,19 @@ const Button = styled(MuiButton)`
 `;
 
 const Stack = styled(MuiStack)`
-    white-space: nowrap;
     -ms-overflow-style: none;  /* Internet Explorer 10+ */
     scrollbar-width: none;  /* Firefox */
     overflow-y: hidden;
     overflow-x: scroll;
     &::-webkit-scrollbar {
         display: none; /* Safari and Chrome */
+    }
+    //解决内容居中后，滚动条左侧不显示全的问题
+    &>button:first-child {
+        margin-left: auto;
+    }
+    &>button:last-child {
+        margin-right: auto;
     }
 `;
 
