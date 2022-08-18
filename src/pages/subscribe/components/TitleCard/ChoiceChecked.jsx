4e4bf494212b5d71d62ca63990ea02616bdd9ec0
wangruirui@ruijie.com.cn <wangruirui>
2022-08-18 11:28:26 +0800

    fix: 增加多选开关控制

--- src/pages/subscribe/components/TitleCard/ChoiceChecked.jsx
+++ src/pages/subscribe/components/TitleCard/ChoiceChecked.jsx
@@ -1,39 +1,56 @@
 import React from 'react';
 import { Box } from "@mui/material";
 import styled, {css} from "styled-components/macro";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { selectSubscribeList } from "@/redux/slices/multiple-choice.slice";
 
-const ChoiceChecked = ({id}) => {
+const ChoiceChecked = ({id, children}) => {
   const subscribeList = useSelector(selectSubscribeList);
-  const isCheck = subscribeList.filter(x => x == id).length > 0;
+  const isCheck = subscribeList.filter(x => x === id).length > 0;
+  const isMultipleMode = subscribeList.length > 0;
   return(
-    <CheckedStyle isCheck={isCheck} />
+    <CheckedStyle isMultipleMode={isMultipleMode} isCheck={isCheck}>
+      {children}
+    </CheckedStyle>
   )
 }
 
 const checkedCss = css`
-  border: 2px solid #f78228;
-  background-color: rgba(0,0,0, 0.3);
+  transform: translateZ(0) scale3d(.79,.84,1);
+  border: 1px solid #f78228;
+  opacity: 1 !important;
+  &:before {
+    filter: blur(0px) opacity(1);
+    
+  }
+  &:after {
+    bottom: 0;
+    right: 0;
+    content: '';
+    position: absolute;
+    width: 0;
+    height: 0;
+    border-bottom: 42px solid #f78228;
+    border-left: 42px solid transparent;
+  }
 `;
 
 const CheckedStyle = styled(Box)`
-  border-radius: 10px;
   width: 100%;
   height: 100%;
-  min-height: 100%;
-  max-height: 100%;
-  ${props => props.isCheck ? checkedCss : null}
-  
+  ${props => props.isCheck ? checkedCss : null};
   position: absolute;
-  top: 0px;
-  right: 0px;
-  bottom: 0px;
-  left: 0px;
-  height: 100%;
-  width: 100%;
   overflow: hidden;
-  
+  transition: opacity,transform .2s ease-out;
+  opacity: ${props => props.isMultipleMode ? 0.5 : 1};
+  &:before {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    content: '';
+  }
 `;
 
 export default ChoiceChecked;
