3681eb3fdc247f35685526fdda55e41835e2296a
yipengfei <yipengfei329@gmail.com>
2022-08-19 18:46:48 +0800

    netflix增加榜单

--- src/pages/recommend/netflixList.jsx
+++ src/pages/recommend/netflixList.jsx
@@ -16,8 +16,10 @@ const NetflixList = () => {
             <FilterOptionsProvider>
                 <DoubanRanking title="热门影片" trendingType="NFPopularonNetflix" trendingName="热门影片"/>
                 <DoubanRanking title="新片上映" trendingType="NFNewReleases" trendingName="新片上映"/>
-                <DoubanRanking title="欧美电影" trendingType="NFWesternMovies" trendingName="欧美电影"/>
-                <DoubanRanking title="欧美剧集" trendingType="NFWesternTVShows" trendingName="欧美剧集"/>
+                <DoubanRanking title="西方电影" trendingType="NFWesternMovies" trendingName="欧美电影"/>
+                <DoubanRanking title="西方剧集" trendingType="NFWesternTVShows" trendingName="欧美剧集"/>
+                <DoubanRanking title="美剧" trendingType="NFUSTVShows" trendingName="美剧"/>
+                <DoubanRanking title="韩剧" trendingType="NFK-Dramas" trendingName="韩剧"/>
                 <DoubanRanking title="纪录片" trendingType="NFDocumentaries" trendingName="纪录片"/>
                 <DoubanRanking title="过去一年推出的影片" trendingType="NFReleasedinthePastYear" trendingName="过去一年推出的影片"/>
             </FilterOptionsProvider>
