da362be4fcbee36ec34f24b804e3333217f5c8c6
pofey@me.com <>
2022-08-30 03:09:27 +0800

    去掉整理的一些配置

--- src/pages/media/Link/LinkDialog/index.jsx
+++ src/pages/media/Link/LinkDialog/index.jsx
@@ -4,8 +4,6 @@ import {Checkbox, FormControl, FormControlLabel, InputLabel, MenuItem, Select, T
 
 const LinkDialog = ({open, onClose, onOk, selectPaths}) => {
     const [linkMode, setLinkMode] = useState("link");
-    const [autoSearchDouban, setAutoSearchDouban] = useState(false);
-    const [doubanProxy, setDoubanProxy] = useState();
     const handleChange = (event) => {
         setLinkMode(event.target.value);
     };
@@ -13,7 +11,7 @@ const LinkDialog = ({open, onClose, onOk, selectPaths}) => {
         <ConfirmDialog
             open={open}
             onClose={onClose}
-            onOk={() => onOk(selectPaths, linkMode, autoSearchDouban,doubanProxy)}
+            onOk={() => onOk(selectPaths, linkMode)}
             content={<>
                 <FormControl sx={{mt: 2}} fullWidth>
                     <InputLabel htmlFor="max-width">转移模式</InputLabel>
@@ -29,30 +27,6 @@ const LinkDialog = ({open, onClose, onOk, selectPaths}) => {
                         <MenuItem value={"move"}>分类后移动到目标目录</MenuItem>
                     </Select>
                 </FormControl>
-                <FormControl sx={{mt: 2}} fullWidth>
-                    <TextField
-                        type="text"
-                        name="doubanProxy"
-                        label="豆瓣代理设置"
-                        value={doubanProxy}
-                        fullWidth
-                        helperText={(
-                            <span>
-                    留空则不使用代理。支持通过HTTP代理、SOCKS代理访问豆瓣（建议试用隧道代理）。示范：http://localhost:8030 或 socks5://user:pass@host:port
-                </span>
-                        )}
-                        onChange={(e) => setDoubanProxy(e.target.value)}
-                        my={3}
-                    />
-                </FormControl>
-                <FormControlLabel
-                    control={<Checkbox
-                        checked={autoSearchDouban}
-                        onChange={(event => setAutoSearchDouban(event.target.checked))}
-                        name="autoSearchDouban"
-                    />}
-                    label="自动搜索豆瓣元数据（数量多时可能会非常慢）"
-                />
             </>}
         >
             您选中了{selectPaths && selectPaths.length}个影片进行刮削，确定要立即开始吗？
