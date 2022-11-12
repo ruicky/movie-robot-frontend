82df6174d61d525eee8a13f435765b7070548395
zkl2333 <zkl2333@gmail.com>
2022-11-12 14:42:31 +0000

    feat: 点击动画

--- src/components/CardButton/index.jsx
+++ src/components/CardButton/index.jsx
@@ -1,10 +1,16 @@
-import styled, {css} from "styled-components/macro";
+import styled, { css, keyframes } from "styled-components/macro";
 import React from "react";
 import AddCircleIcon from '@mui/icons-material/AddCircle';
-import {Box as MuiBox, Typography} from "@mui/material";
+import { Box as MuiBox, Typography } from "@mui/material";
 import _ from "lodash";
 import * as m_icon from "@mui/icons-material";
 import * as f_icon from "react-feather";
+
+const breatheAnimation = keyframes`
+  0% { transform: scale(1); }
+  50% { transform: scale(0.98); }
+  100% { transform: scale(1); }
+`
 
 const cardSelectStyle = css`
   border-style:solid;
@@ -23,6 +29,10 @@ const Card = styled.div`
   -webkit-mask-image: paint(squircle);
   --squircle-radius: 20px;
   --squircle-smooth: 0.8;
+  &.isAnimation {
+    animation-name: ${breatheAnimation};
+    animation-duration: 0.3s;
+  }
   ${(props) => props.selected && cardSelectStyle}
 `;
 const CardHead = styled(MuiBox)({
@@ -30,15 +40,33 @@ const CardHead = styled(MuiBox)({
     justifyContent: 'space-between',
     marginBottom: 20
 });
-export const CardButton = ({color, icon, label, helper, selected, onClick}) => {
+export const CardButton = ({ color, icon, label, helper, selected, onClick }) => {
     const Icon = _.get({
         ...m_icon,
         ...f_icon
     }, icon, null);
-    return <Card color={color} selected={selected} onClick={onClick}>
+
+    // 点击动画
+    const [isAnimation, setIsAnimation] = React.useState(false);
+
+
+    const handleClick = (event) => {
+        setIsAnimation(true);
+        event.currentTarget.addEventListener('animationend', () => {
+            setIsAnimation(false);
+        });
+        onClick && onClick(event);
+    }
+
+    return <Card
+        color={color}
+        selected={selected}
+        onClick={handleClick}
+        className={isAnimation && 'isAnimation'}
+    >
         <CardHead>
-            {Icon&&<Icon fontSize="large"/>}
-            <AddCircleIcon fontSize="large"/>
+            {Icon && <Icon fontSize="large" />}
+            <AddCircleIcon fontSize="large" />
         </CardHead>
         <Typography variant="h6" color="#FFF" gutterBottom>
             {label}
