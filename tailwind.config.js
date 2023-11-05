c81b0b21258dd6bd34855fd69157106173334988
zkl2333 <i@zkl2333.com>
2023-11-05 12:10:37 +0800

    fix: 🐛 修复 MUI Tailwind 兼容问题

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+// https://mui.com/material-ui/guides/interoperability/#tailwind-css
 module.exports = {
+  // Remove Tailwind CSS's preflight style so it can use the Material UI's preflight instead (CssBaseline).
+  corePlugins: {
+    preflight: false,
+  },
+  important: "#root",
   prefix: "tw-",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
